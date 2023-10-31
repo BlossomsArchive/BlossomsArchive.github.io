5137d942a1add91ab3034c167cd8ce5f0425de16
MizuiMiduki <6161.k.iga@gmail.com>
2023-10-31 11:04:01 +0900

    20231031-3

---
+++ common/common.js
@@ -0,0 +1,6 @@
+$(function() {
+    $("#header").load("/parts/html/header.html");
+    $("#footer").load("/parts/html/footer.html");
+    $("#side-menu").load("/parts/html/side-menu.html");
+    $("#side-menu-sp").load("/parts/html/side-menu.html");
+   });
